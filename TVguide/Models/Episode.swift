92674f3e6b9139e8160b421eae49b408842b7561
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-15 05:47:04 -0500

    Add Episode model object

---
+++ TVguide/Models/Episode.swift
@@ -0,0 +1,15 @@
+//
+//  Episode.swift
+//  TVguide
+//
+//  Created by Ilgar Ilyasov on 6/15/19.
+//  Copyright © 2019 IIIyasov. All rights reserved.
+//
+
+import Foundation
+
+struct Episode: Decodable {
+    let day: String
+    let hour: String
+    let title: String
+}
