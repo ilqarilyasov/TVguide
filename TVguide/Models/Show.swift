283f52cbb89ca163d2db2a0cb2c8027f6d1e8b26
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-15 05:46:52 -0500

    Add Show model object

---
+++ TVguide/Models/Show.swift
@@ -0,0 +1,32 @@
+//
+//  Show.swift
+//  TVguide
+//
+//  Created by Ilgar Ilyasov on 6/15/19.
+//  Copyright © 2019 IIIyasov. All rights reserved.
+//
+
+import Foundation
+
+struct Show: Decodable {
+    let identifier: Int
+    let imageURL: String
+    let link: String
+    let name: String
+    let tvChannel: String
+    
+    let description: String?
+    let episodes: [Episode]?
+    
+    enum CodingKeys: String, CodingKey {
+        case identifier = "num"
+        case imageURL = "img"
+        case link = "lnk"
+        case name = "name"
+        case tvChannel = "air"
+        case description = "desc"
+        case episodes = "listings"
+    }
+}
+
+typealias TVShows = [Show]
