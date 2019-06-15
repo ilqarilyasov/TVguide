94ab14c9b6f6a9d0fbe8366a8bc810199c17143b
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-15 07:18:29 -0500

    Add a concurrent subclass of Operation

---
+++ TVguide/Operations/ConcurrentOperation.swift
@@ -0,0 +1,51 @@
+//
+//  ConcurrentOperation.swift
+//  TVguide
+//
+//  Created by Ilgar Ilyasov on 6/15/19.
+//  Copyright © 2019 IIIyasov. All rights reserved.
+//
+
+import Foundation
+
+class ConcurrentOperation: Operation {
+    
+    // MARK: Types
+    
+    enum State: String {
+        case isReady, isExecuting, isFinished
+    }
+    
+    // MARK: Properties
+    
+    var state = State.isReady {
+        willSet {
+            willChangeValue(forKey: newValue.rawValue)
+            willChangeValue(forKey: state.rawValue)
+        }
+        didSet {
+            didChangeValue(forKey: oldValue.rawValue)
+            didChangeValue(forKey: state.rawValue)
+        }
+    }
+    
+    // MARK: NSOperation
+    
+    override dynamic var isReady: Bool {
+        return super.isReady && state == .isReady
+    }
+    
+    override dynamic var isExecuting: Bool {
+        return state == .isExecuting
+    }
+    
+    override dynamic var isFinished: Bool {
+        return state == .isFinished
+    }
+    
+    override var isAsynchronous: Bool {
+        return true
+    }
+    
+}
+
